6ed6d5bdc8274b977adb10fd3c11386f806b3505
imingasco <imingasco123@gmail.com>
2022-05-08 02:04:05 +0800

    Dot product done

--- workshop/opencl/dot_product/main.c
+++ workshop/opencl/dot_product/main.c
@@ -1,81 +1,84 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <inttypes.h>
 #include <assert.h>
+#include <inttypes.h>
+#include <string.h>
 #include <CL/cl.h>
-#include <omp.h>
-
-#define MAXDEVICE 16 
+#include "utils.h"
+#define MAXGPU 1
 #define MAXN 16777216
-#define GROUPSIZE 128
-
-const char filename[16] = "vecdot.cl";
-const char kernel_name[16] = "vecdot";
-uint32_t vec[MAXN];
-cl_platform_id platform;
-cl_device_id device;
+#define GPULOCAL 256
+uint32_t vec[MAXN/GPULOCAL];
+int N;
+uint32_t keyA, keyB;
+char source[1024] = "";
+// -- start working with OpenCL
 cl_context context;
-cl_command_queue queue;
 cl_program prog;
 cl_kernel kernel;
-cl_mem buffer;
+cl_command_queue queue;
+cl_mem mem;
 
-int OpenCLInit() {
-    cl_device_id devices[MAXDEVICE];
+int initAllGPU() {
+    // -- generate kernel code
+    FILE *fp = fopen("vecdot.cl", "r");
+    size_t source_len = fread(source, 1, 1024, fp);
     cl_int status;
-    cl_uint platform_num, device_num;
-
-    status = clGetPlatformIDs(1, &platform, &platform_num);
-
-    status = clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, MAXDEVICE, devices, &device_num);
-    if (status == CL_SUCCESS) {
-        FILE *fp = fopen(filename, "r");
-        char source[1024] = "";
-        const char *src_ptr = source;
-        size_t len = fread(source, sizeof(char), 1024, fp);
-        device = devices[0];
-        context = clCreateContext(NULL, 1, &device, NULL, NULL, &status);
-        queue = clCreateCommandQueueWithProperties(context, device, NULL, &status);
-        prog = clCreateProgramWithSource(context, 1, &src_ptr, &len, &status);
-        status = clBuildProgram(prog, 1, &device, NULL, NULL, NULL);
-        kernel = clCreateKernel(prog, kernel_name, &status);
-        buffer = clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(uint32_t) * MAXN, vec, &status);
-        return 0;
-    }
+    cl_uint platform_n, device_n;
+    cl_platform_id platform;
+    cl_device_id device;
+    const char *src_ptr = source;
+    // -- basic OpenCL setup
+    clGetPlatformIDs(1, &platform, &platform_n);
+    clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, 1, &device, NULL);
+    context = clCreateContext(NULL, 1, &device, NULL, NULL, &status);
+    queue = clCreateCommandQueueWithProperties(context, device, NULL, &status);
+    prog = clCreateProgramWithSource(context, 1, &src_ptr, &source_len, &status);
+    status = clBuildProgram(prog, 1, &device, NULL, NULL, NULL);
+    assert(status == CL_SUCCESS);
+    kernel = clCreateKernel(prog, "vecdot", &status);
+    // -- create all buffers
+    cl_mem_flags flag = CL_MEM_WRITE_ONLY | CL_MEM_COPY_HOST_PTR;
+    mem = clCreateBuffer(context, flag, sizeof(uint32_t) * (MAXN / GPULOCAL), vec, &status);
     return 1;
 }
 
-void OpenCLClean() {
-    clReleaseMemObject(buffer);
-    clReleaseKernel(kernel);
-    clReleaseProgram(prog);
-    clReleaseCommandQueue(queue);
-    clReleaseContext(context);
+int executeGPU() {
+    uint32_t padding = 0;
+    while (N % GPULOCAL) {
+        padding += encrypt(N, keyA) * encrypt(N, keyB);
+        N++;
+    }
+    cl_int clStat;
+    size_t globalOffset[] = {0};
+    size_t globalSize[] = {N};
+    size_t localSize[] = {GPULOCAL};
+    
+    // -- set argument to kernel
+    clStat = clSetKernelArg(kernel, 0, sizeof(cl_uint), (void *) &keyA);
+    clStat = clSetKernelArg(kernel, 1, sizeof(cl_uint), (void *) &keyB);
+    clStat = clSetKernelArg(kernel, 2, sizeof(cl_mem), (void *) &mem);
+    // -- execute
+    clStat = clEnqueueNDRangeKernel(queue, kernel, 1, globalOffset,
+            globalSize, localSize, 0, NULL, NULL);
+    
+    // -- read back
+    clEnqueueReadBuffer(queue, mem, CL_TRUE, 0, sizeof(uint32_t) * (N / GPULOCAL), vec, 0, NULL, NULL);
+    // clFinish(queue);
+    uint32_t sum = 0;
+    for (int i = 0; i < N / GPULOCAL; i++)
+        sum += vec[i];
+    printf("%u\n", sum - padding);
+    return 1;
 }
 
-int main(int argc, char **argv) {
-    int N;
-    uint32_t key1, key2;
-
-    omp_set_num_threads(4);
-    OpenCLInit();
-    while (scanf("%d %" PRIu32 " %" PRIu32, &N, &key1, &key2) == 3) {
-        size_t global_offset[] = {0};
-        size_t global_size[] = {N};
-        size_t work_item_per_work_group[] = {1};
-        clSetKernelArg(kernel, 0, sizeof(cl_uint), (void *) &key1);
-        clSetKernelArg(kernel, 1, sizeof(cl_uint), (void *) &key2);
-        clSetKernelArg(kernel, 2, sizeof(cl_mem), (void *) buffer);
-        clEnqueueNDRangeKernel(queue, kernel, 1, global_offset, global_size, work_item_per_work_group, 0, NULL, NULL);
-        clEnqueueReadBuffer(queue, buffer, CL_TRUE, 0, sizeof(uint32_t) * N, vec, 0, NULL, NULL);
-
-        uint32_t sum = 0;
-#pragma omp parallel for reduction(+ : sum)
-        for (int i = 0; i < N; i++) {
-            sum += vec[i];
-        }
-        printf("%u\n", sum);
+void onStart() {
+    initAllGPU("vecdot.cl");
+    while (scanf("%d %u %u", &N, &keyA, &keyB) == 3) {
+        executeGPU();
     }
-    OpenCLClean();
+}
+
+int main(int argc, char *argv[]) {
+    onStart();
     return 0;
 }
